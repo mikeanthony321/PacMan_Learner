994ea0e72288faf6d4a9f14db0eb9061447938c4
Kyle McKinley <kmckinley@protonmail.com>
2021-01-31 23:07:55 -0500

    Added Exec Timer
    Added an Execution Timer and made final touches to Sprint 1 components in the Analytics window.

--- game/analytics.py
+++ game/analytics.py
@@ -1,33 +1,81 @@
 from settings import *
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QPushButton, QLabel, QTableWidget, QTableWidgetItem
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QFont
 
 class Analytics(QMainWindow):
     def __init__(self, monitor_size):
         super().__init__()
         self.window = QMainWindow()
         self.window.resize(WIDTH, HEIGHT)
-        #PyQt5 vs PyGame do not position from the same spot and I couldn't for the life of me find what the exact
-        #height of the top bar of the PyQt5 window is by default and had to mess around with values to find 31. Problem
-        #is that I can't guarantee this isn't different on different OS, so this is more a temp measure.
         self.window.move(monitor_size.width() / 2 - WIDTH, monitor_size.height() / 2 - HEIGHT / 2 - 31)
         self.window.setWindowTitle('Pac-Man Learner Analytics')
 
         self.running = False
+        self.tar_high_score = 0
+        self.learning_rate = 0.0
+
+        self.timer_min = 0
+        self.timer_sec = 0
+        self.timer_ms = 0
+
+        self.start_screen()
 
 # -- -- -- GENERAL FUNCTIONS -- -- -- #
-    def buttonClicked(self):
-        self.running = not self.running
-        print(self.running)
-
     def updateScreen(self):
         if not self.running:
             self.start_screen()
         else:
             self.sim_screen()
 
+    def showTime(self):
+        if self.running:
+            self.timer_sec += 1
+            if self.timer_sec >= 60:
+                self.timer_min += 1
+                self.timer_sec = 0
+
+            self.formatTime()
+
+    def formatTime(self):
+        if self.timer_sec < 10:
+            self.timer_label.setText("Execution timer: %d:0%d" % (self.timer_min, self.timer_sec))
+        else:
+            self.timer_label.setText("Execution timer: %d:%d" % (self.timer_min, self.timer_sec))
+
+# -- -- -- BUTTON FUNCTIONS -- -- -- #
+    def buttonClicked(self):
+        self.timer.start(1000)
+        self.sim_screen()
+        self.running = True
+
+    def highScoreButton(self):
+        if not self.running:
+            self.tar_high_score = self.tar_high_score_input.text()
+            self.tar_high_score = int(self.tar_high_score)
+            print("The target high score is: " + self.tar_high_score_input.text())
+            self.tar_high_score_input.setText("")
+        else:
+            print("You must stop the sim to enter a target high score")
+            self.tar_high_score_input.setText("")
+
+    def learningRateButton(self):
+        if not self.running:
+            self.learning_rate = self.learning_rate_input.text()
+            self.learning_rate = float(self.learning_rate)
+            if self.learning_rate < 1.0:
+                print(self.learning_rate)
+            else: #I do not know enough about learning rate to know if we want to require it be under 1, just did to have a test
+                print('Please enter a number less than 1')
+            self.learning_rate_input.setText("")
+        else:
+            print("You must stop the sim to enter a new learning rate")
+            self.learning_rate_input.setText("")
+
 
 # -- -- -- START MENU FUNCTIONS -- -- -- #
     def start_screen(self):
+        #Create the Label/Text Input/Button for the Target High Score
         self.tar_high_score_label = QLabel('Target High Score', self.window)
         self.tar_high_score_label.move(20, 35)
         self.tar_high_score_input = QLineEdit(self.window)
@@ -36,6 +84,7 @@ class Analytics(QMainWindow):
         self.tar_high_score_button = QPushButton('Submit', self.window)
         self.tar_high_score_button.move(170, 60)
 
+        #Create the Label/Text Input/Button for the Learning Rate
         self.learning_rate_label = QLabel('Learning Rate', self.window)
         self.learning_rate_label.move(20, 105)
         self.learning_rate_input = QLineEdit(self.window)
@@ -44,13 +93,27 @@ class Analytics(QMainWindow):
         self.learning_rate_button = QPushButton('Submit', self.window)
         self.learning_rate_button.move(170, 130)
 
+        #Create the Label/Button for the Begin button to start the game (sim)
         self.begin_label = QLabel('Begin Sim', self.window)
         self.begin_label.move(170, 250)
         self.begin_button = QPushButton('Begin', self.window)
         self.begin_button.move(170, 275)
         self.begin_button.clicked.connect(self.buttonClicked)
 
+        #Create the Label and Timer for the Execution Timer
+        #This would actually be present on the sim screen, not the main screen but for now it'll live on the main screen
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.showTime)
+        self.timer_label = QLabel('Execution timer: 0:00', self.window)
+        self.timer_label.move(380, 35)
+        self.timer_label.resize(150,50)
+        self.timer_label.setFont(QFont('Arial', 12))
+
+        self.tar_high_score_button.clicked.connect(self.highScoreButton)
+        self.learning_rate_button.clicked.connect(self.learningRateButton)
         self.window.show()
+
+
 
 # -- -- -- SIM RUNNING FUNCTIONS -- -- -- #
     def sim_screen(self):
--- game/pacman.py
+++ game/pacman.py
@@ -31,7 +31,7 @@ class Pacman:
                 self.running = False
 
             #This is a temporary section meant to test interaction (albeit a simple interaction)
-            self.analytics.updateScreen()
+            #self.analytics.updateScreen()
             if self.analytics.running:
                 self.state = 'game'
 
@@ -41,6 +41,9 @@ class Pacman:
         if self.player.score >= int(HIGH_SCORE):
             open("db/hs.txt", "w").write(str(self.player.score))
         # todo: this ^ is just a mock high score counter, it adds 1 to the high score in db on each exit
+
+        if self.player.score >= int(self.analytics.tar_high_score):
+            print("Target High Score Achieved!")
         pygame.quit()
         sys.exit()
 
